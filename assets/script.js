894bc1964d82c85df301badfa03ddfb56c3dee5b
Jesse Coad <jessecoad@gmail.com>
2021-05-27 23:58:39 +1000

    function

--- assets/script.js
+++ assets/script.js
@@ -109,3 +109,66 @@ function questionStart() {
     indexNum++;
   }
 }
+
+// answer saver
+var answerCheck = function (event) {
+  var guess1 = event.target;
+  if (guess1.matches(".guess-list")) {
+    var guessActual = guess1.getAttribute("btn-id");
+    guessCompare(guessActual);
+  }
+};
+
+// function to compare the guess to the actual answer
+var guessCompare = function (guessId) {
+  // if guessed correct
+  if (guessId === answer) {
+    // add to timer, increase correct var, display correct, run next question
+    timer += 3;
+    correct++;
+    resultEl.innerText = "Correct!";
+    runQuiz();
+    // if guessed incorrect
+  } else {
+    // subtract from timer, increase wrong var, display wrong, run next question
+    timer -= 10;
+    wrong++;
+    resultEl.innerText = "Wrong!";
+    runQuiz();
+  }
+};
+
+// function to endQuiz when all questions answered or timer runs out
+var endQuiz = function () {
+  // ensure score can't be negative
+  if (timer < 0) {
+    timer = 0;
+    timerEl.innerText = timer;
+  }
+  // update DOM
+  questionEl.removeAttribute("style");
+  questionEl.textContent = "Let's see how you did!";
+  messageEl.innerHTML = `<div>You got ${correct} questions correct and ${wrong} questions wrong.</div><div>Your time score is: ${timer}.</div>`;
+  // create and append a form elements for submitting initials
+  var formEl = document.createElement("form");
+  formEl.setAttribute("id", "initials-form");
+  var inputEl = document.createElement("input");
+  inputEl.setAttribute("type", "text");
+  inputEl.setAttribute("name", "user-initials");
+  inputEl.className = "user-initials";
+  inputEl.setAttribute("placeholder", "Enter Your Initials");
+  formEl.appendChild(inputEl);
+  var submitEl = document.createElement("button");
+  submitEl.className = "btn";
+  submitEl.setAttribute("id", "save-initials");
+  submitEl.setAttribute("type", "submit");
+  submitEl.textContent = "Submit";
+  formEl.appendChild(submitEl);
+  // append the entire form to the messageEl
+  messageEl.appendChild(formEl);
+};
+
+// event listener for click of start button
+startBtn.addEventListener("click", startQuiz);
+// event listener for click of a guess button during quiz
+messageEl.addEventListener("click", guessHandler);
